0aeebd55ec9217160f332c14d6d20ea35783df04
Stepka <micromillioner@yahoo.com>
2026-04-14 10:49:30 +0200

    leads

---
+++ backend/api/lead.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, requests
+from pydantic import BaseModel
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+router = APIRouter(prefix="/lead", tags=["lead"])
+
+TELEGRAM_TOKEN = os.getenv("TG_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TG_CHAT_ID")
+
+class Lead(BaseModel):
+    name: str
+    phone: str
+    email: str = ""
+    message: str = ""
+
+@router.post("/send")
+def send_lead(data: Lead):
+    text = f"""
+Новая заявка:
+
+Имя: {data.name}
+Телефон: {data.phone}
+Email: {data.email}
+Сообщение: {data.message}
+"""
+
+    msg = MIMEText(text)
+    msg["Subject"] = "Новая заявка AI-TV"
+    msg["From"] = os.getenv("GMAIL_APP_MAIL")
+    msg["To"] = os.getenv("GMAIL_APP_MAIL")
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(os.getenv("GMAIL_APP_MAIL"), os.getenv("GMAIL_APP_PASS"))
+    server.send_message(msg)
+    server.quit()
+
+    return {"status": "ok"}
+
+def send_to_telegram(text: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+    requests.post(url, json={
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": text
+    })
